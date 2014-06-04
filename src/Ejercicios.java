522032a839441c926a2c837bc95491d3cfb7979c
Turupawn <turupawn@userr>
2014-06-04 10:36:18 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,68 @@
+
+public class Ejercicios {
+	
+	//devuelve la suma de a y b
+	static int sumarNumeros(int a, int b)
+	{
+		return -1;
+	}
+	
+	//devuelve la resta de a y b
+	static int restarNumeros(int a, int b)
+	{
+		return -1;
+	}
+	
+	//devuelve la multiplicacion de a y b
+	static int multiplicarNumeros(int a, int b)
+	{
+		return -1;
+	}
+	
+	//devuelve la division de a y b
+	static int dividirNumeros(int a, int b)
+	{
+		return -1;
+	}
+	
+	//devuelve el primer numero de un archivo
+	static int getPrimerNumeroDeArchivo(String path)
+	{
+		return -1;
+	}
+	
+	//devuelve el ultimo numero de un archivo
+	static int getUltimoNumeroDeArchivo(String path)
+	{
+		return -1;
+	}
+	
+	//Suma todos los numeros del archivo
+	static int sumarNumerosDeArchivo(String path)
+	{
+		return -1;
+	}
+	
+	//Devuelve true si encuentra un numero en un archivo, de lo contrario devuelve false
+	static boolean existeEnArchivo(String path,int num)
+	{		
+		return false;
+	}
+	
+	//Devuelve el numero menor de un archivo
+	static int getMenorDeArchivo(String path)
+	{
+		return -1;
+	}
+	
+	//Devuelve el numero mayor de un archivo
+	static int getMayorDeArchivo(String path)
+	{
+		return -1;
+	}
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
